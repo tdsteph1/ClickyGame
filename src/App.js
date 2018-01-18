b958b6babae42b8147dd79a4de6e86f25f496499
Your Name Here <tdsteph1@asu.edu>
2018-01-18 14:26:44 -0700

    images DIR

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import Game from "./components/Game"
-//import Wrapper from "./components/Wrapper";
-//import Navbar from "./components/Navbar";
-//import Jumbotron from "./components/Jumbotron";
-//import Container from "./components/Container";
-//import Row from "./components/Row";
-//import Col from "./components/Col";
+import Game from "./components/Game";
+
 
 
 const App = () => <Game />
